a29d6b49327f85848ec873ec3a6a49963a8c5a0d
Jose Luis Rey Mendez <jlreymendez@gmail.com>
2020-08-14 11:14:32 -0300

    Moved config data to a scriptable object

---
+++ UECS/Assets/Code/Data/SimulationConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace AntPheromones.Data
+{
+    [CreateAssetMenu(fileName = "SimulationConfig", menuName = "Simulation/Config", order = 0)]
+    public class SimulationConfig : ScriptableObject
+    {
+        public uint Seed { get => _seed == 0 ? (uint) Random.Range(int.MinValue, int.MaxValue) : _seed; }
+        [SerializeField] uint _seed;
+
+        public int MapSize = 128;
+        public int BucketResolution = 64;
+
+        public int ObstacleRingsCount = 3;
+        [Range(0f, 1f)] public float ObstaclesPerRing = 0.8f;
+    }
+}
--- UECS/Assets/Code/Obstacles/Systems/SpawningObstaclesSystem.cs
+++ UECS/Assets/Code/Obstacles/Systems/SpawningObstaclesSystem.cs
@@ -1,41 +1,41 @@
-﻿using Unity.Collections;
+﻿using AntPheromones.Data;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Random = Unity.Mathematics.Random;
+using Task = System.Threading.Tasks.Task;
 
 namespace AntPheromones.Obstacles.Systems
 {
     public class SpawningObstaclesSystem : SystemBase
     {
-        static readonly int _obstacleRings = 3;
-        static readonly float _obstaclesPerRing = 0.8f;
-        static readonly float _obstacleRadius = 2f;
-        static readonly int _mapSize = 128;
-        static readonly int _bucketResolution = 64;
-        static readonly uint _seed = 4;
-
         protected override async void OnStartRunning()
         {
+            var configLoader = Addressables.LoadAssetAsync<SimulationConfig>("SimulationConfig");
             var obstacleLoader = Addressables.LoadAssetAsync<GameObject>("ObstaclePrefab");
-            await obstacleLoader.Task;
-            CreateObstacleEntities(obstacleLoader.Result);
+            await Task.WhenAll(configLoader.Task, obstacleLoader.Task);
+            CreateObstacleEntities(obstacleLoader.Result, configLoader.Result);
         }
 
-        void CreateObstacleEntities(GameObject obstaclePrefab)
+        void CreateObstacleEntities(GameObject obstaclePrefab, SimulationConfig config)
         {
             var gameConversionSettings = GameObjectConversionSettings.FromWorld(World, null);
             var entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(obstaclePrefab, gameConversionSettings);
-            var random = new Random(_seed);
+            var random = new Random(config.Seed);
+
+            var obstacleRings = config.ObstacleRingsCount;
+            var obstaclesPerRing = config.ObstaclesPerRing;
+            var mapSize = config.MapSize;
+            var bucketResolution = config.BucketResolution;
 
             var obstacleRadius = EntityManager.GetComponentData<Radius>(entityPrefab).Value;
-            var scale = new float3(obstacleRadius * 2, obstacleRadius * 2, 1) / _mapSize;
+            var scale = new float3(obstacleRadius * 2, obstacleRadius * 2, 1) / mapSize;
 
-            for (var i = 1; i <= _obstacleRings; i++)
+            for (var i = 1; i <= obstacleRings; i++)
             {
-                var ringRadius = (i / (_obstacleRings + 1f)) * (_mapSize * .5f);
+                var ringRadius = (i / (obstacleRings + 1f)) * (mapSize * .5f);
                 var circumference = ringRadius * 2f * math.PI;
                 var maxCount = (int)math.ceil(circumference / (2f * obstacleRadius) * 2f);
                 var offset = random.NextInt(0, maxCount);
@@ -44,20 +44,20 @@ namespace AntPheromones.Obstacles.Systems
                 for (int j = 0; j < maxCount; j++)
                 {
                     var t = (float)j / maxCount;
-                    if ((t * holeCount) % 1f < _obstaclesPerRing)
+                    if ((t * holeCount) % 1f < obstaclesPerRing)
                     {
                         var angle = (j + offset) / (float)maxCount * (2f * Mathf.PI);
-                        var position = new float3(_mapSize * .5f + Mathf.Cos(angle) * ringRadius, _mapSize * .5f + Mathf.Sin(angle) * ringRadius, 0);
-                        // Debug.DrawRay(position / _mapSize,-Vector3.forward * .05f,Color.green,10000f);
+                        var position = new float3(mapSize * .5f + Mathf.Cos(angle) * ringRadius, mapSize * .5f + Mathf.Sin(angle) * ringRadius, 0);
+                        // Debug.DrawRay(position / mapSize,-Vector3.forward * .05f,Color.green,10000f);
                         var obstacle = EntityManager.Instantiate(entityPrefab);
-                        EntityManager.AddComponentData(obstacle, new Translation { Value = position / _mapSize });
+                        EntityManager.AddComponentData(obstacle, new Translation { Value = position / mapSize });
                         EntityManager.AddComponentData(obstacle, new NonUniformScale { Value = scale });
 
                         EntityManager.AddComponentData(obstacle, new MapBucket
                         {
                             Position = new int2(
-                                (int)math.floor((position.x - obstacleRadius) / _mapSize * _bucketResolution),
-                                (int)math.floor((position.y - obstacleRadius) / _mapSize * _bucketResolution)
+                                (int)math.floor((position.x - obstacleRadius) / mapSize * bucketResolution),
+                                (int)math.floor((position.y - obstacleRadius) / mapSize * bucketResolution)
                             )
                         });
                     }
