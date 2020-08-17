1d76e65401451eeed906ea77e4536440ef155d8c
Jose Luis Rey Mendez <jlreymendez@gmail.com>
2020-08-17 12:16:48 -0300

    Added bucket data helper struct

---
+++ UECS/Assets/Code/Common/Utils/BucketData.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Mathematics;
+
+namespace AntPheromones.Common
+{
+    public struct BucketData
+    {
+        public int BucketResolution;
+
+        public BucketData(int bucketResolution)
+        {
+            BucketResolution = bucketResolution;
+        }
+
+        public int2 GetBucket(float3 position)
+        {
+            return new int2(
+                (int) math.floor(position.x * BucketResolution),
+                (int) math.floor(position.y * BucketResolution)
+            );
+        }
+    }
+}
