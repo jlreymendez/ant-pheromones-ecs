a6ae3ef138adac2716009348006113e5d662fb32
Jose Luis Rey Mendez <jlreymendez@gmail.com>
2020-08-17 18:16:52 -0300

    Added control simulation speed with alpha keys from 1 to 9

---
+++ UECS/Assets/Code/Input/Systems/SimulationInputSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UInput = UnityEngine.Input;
+
+namespace AntPheromones.Input
+{
+    public class SimulationInputSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            for (var i = 1; i < 10; i++)
+            {
+                if (UInput.GetKeyUp((KeyCode)((int)KeyCode.Alpha0 + i)))
+                {
+                    UnityEngine.Time.timeScale = i;
+                }
+            }
+
+        }
+    }
+}
